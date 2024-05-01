c799fcc024941e8ee58aa7fae7464b8de58394e3
MR2611 <ruhullah258@gmail.com>
2024-05-01 22:24:24 +0530

    new changes added.

--- Database/db.js
+++ Database/db.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 
 const mongoURL = process.env.MONGODB_URL;
 
-mongoose.connect(mongoURL);
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 
 const db = mongoose.connection;
 
